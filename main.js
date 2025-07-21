6fefd515f0af09bf059bb346c0ecff8d6fd4a12d
ACE <bdearrabea320@gmail.com>
2025-07-21 20:56:59 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 let display = document.getElementById('user-input');
+let lastWasResult = false;
 
 
 
 
 function appendtodisplay (input){
- display.value += input;
-   
+const isOperator = ['+','-','*','/'].includes(input);
 
 
+if (lastWasResult) {
+    if ( isOperator){
+        lastWasResult = false;
+    }else{
+        display.value = ""; 
+        lastWasResult = false;
+    }
+        
+        
+    }
+    display.value += input;
 }
+
+
+
 function cleardisplay(){
  
     display.value = " ";
-    
+     lastWasResult = false;
 }
 
 function calc() {
@@ -20,8 +34,11 @@ function calc() {
         let cleanedInput = display.value.replace(/\b0+(\d+)/g, '$1');
         
         display.value = eval(cleanedInput);
+         lastWasResult = true;
     } catch {
         display.value = "Error";
+        lastWasResult = false;
+
     }
 }
 
